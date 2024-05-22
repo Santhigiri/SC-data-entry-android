05da2380f417bc22091aa27e8c79199ee263dfab
kamalnathvg <kamalnath7171@gmail.com>
2024-05-21 22:24:31 -0400

    added getDate middleware template

--- app/src/main/java/com/example/sc_data_entry/state/AppMiddleWares.kt
+++ app/src/main/java/com/example/sc_data_entry/state/AppMiddleWares.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.reduxkotlin.thunk.Thunk
+import java.time.LocalDate
 
 
 fun addPrayerTimeThunk(coroutineScope: CoroutineScope, prayerTime: String) :Thunk<AppState> = { dispatch, _, _ ->
@@ -13,5 +14,12 @@ fun addPrayerTimeThunk(coroutineScope: CoroutineScope, prayerTime: String) :Thun
         delay(3000)//mimicking api call
         dispatch(AppActions.DataFetchingStopped)
         dispatch(AppActions.AddSpecialPrayer(prayerTime = prayerTime))
+    }
+}
+
+fun fetchDataByDateThunk(coroutineScope: CoroutineScope,date: LocalDate): Thunk<AppState> = {dispatch , _, _ ->
+    dispatch(AppActions.DataFetchingStarted)
+    coroutineScope.launch(Dispatchers.IO) {
+        //TODO: Make api call to fetch data
     }
 }
